d33d7c5e66fe0840f21405a872bb90d77188b87d
Jussi Kinnunen <jussikin@iki.fi>
2015-05-03 12:27:26 +0300

    first working version of node.js receiwer

--- nrfReceiver/receiver.js
+++ nrfReceiver/receiver.js
@@ -1,5 +1,10 @@
 var nrf = require('nrf')
 var radio = nrf.connect("/dev/spidev0.0", 18)
+var mqtt    = require('mqtt');
+var mqclient  = mqtt.connect();
+var redis = require("redis"),
+redisclient = redis.createClient();
+require('buffertools').extend();
 
 radio.channel(0,function(err){
 	if(err)console.log(err)
@@ -12,12 +17,17 @@ radio.begin(function () {
    console.log("radio ready, begin receiwing")
     var rx = radio.openPipe('rx',0x6161616161,{size:5,autoAck:false})
     rx.on('data', function(chunk) {
-	console.log("chunkki sissä")
-    	console.log(chunk)
+	chunk.reverse()
+	//console.log(chunk)
+	var asHex = chunk.toString('hex')
+	redisclient.publish('radiodata',asHex)
+	mqclient.publish('radio/data',asHex)
+    	console.log(asHex+":"+new Date())
     });
 });
 
-
+/*
 setInterval(function(){
 	radio.printDetails();
 }, 20000)
+*/
