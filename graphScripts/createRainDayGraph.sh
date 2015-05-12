63b1a3cc63ef046f88b0be9eb46de2bc45ebb288
Jussi Kinnunen <jussikin@iki.fi>
2015-05-12 19:25:15 +0300

    Rain graph fixes

--- graphScripts/createRainDayGraph.sh
+++ graphScripts/createRainDayGraph.sh
@@ -33,6 +33,9 @@ rrdtool graph - \
  -A -l 900 -u 1100 \
  -L 10 -X 0 \
  DEF:b=/home/jussikin/rrd/ulkopaine.rrd:preassure:AVERAGE \
+ DEF:d=/home/jussikin/rrd/sisapaine.rrd:preassure:AVERAGE \
  CDEF:c=b,100,/ \
- "LINE:c#0000FF:paine"> $OutDir/preassure.png
+ CDEF:e=d,100,/ \
+ "LINE:e#FF00FF:sisapaine" \
+ "LINE:c#0000FF:ulkopained"> $OutDir/preassure.png
 
